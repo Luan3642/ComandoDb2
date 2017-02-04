048dbfbdd13ec77d7840b467f1bf4196076ffb9d
Luan3642 <luan3642@hotmail.com>
2017-02-04 11:29:20 -0200

    - Comandos Db2

---
+++ src/comandosdb2/ComandosDb2.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comandosdb2;
+
+/**
+ *
+ * @author luan
+ */
+public class ComandosDb2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // CRIANDO UMA BASE 
+        //db2 create database escola
+
+        // CONECTANDO A BASE 
+        //db2 connect to escola
+
+       // CRIANDO UMA TABELA 
+       // db2 create table pessoa (id int, nome varchar(50), idade int, sexo varchar(50)) 
+       
+       // INSERINDO NA TABLE 
+      // db2 insert into pessoa values ('1', 'luan', '16', 'masculino')
+      
+      // APAGANDO A TABLE 
+     // db2 drop table pessoa 
+    }
+
+}
